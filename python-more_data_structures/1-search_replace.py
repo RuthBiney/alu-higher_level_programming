aeee9e62e1e1d61043e81680fdd418b1f7e05ca2
RuthBiney <r.biney@alustudent.com>
2022-11-04 02:03:45 -0700

    Search

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# A function that replaces all occurrences of an element
+# by another in a new list
+
+
+def search_replace(my_list, search, replace):
+    new_list = []
+    for i in range(len(my_list)):
+        if my_list[i] == search:
+            new_list.append(replace)
+        else:
+            new_list.append(my_list[i])
+    return new_list
