6ecde68be349b18b46864769f4f1fe052b013844
RuthBiney <r.biney@alustudent.com>
2022-11-04 02:40:13 -0700

    number

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# A function that returns the number of keys in a dictionary
+
+
+def number_keys(a_dictionary):
+    num = 0
+    list_keys = list(a_dictionary.keys())
+
+    for i in list_keys:
+        num += 1
+
+    return (num)
