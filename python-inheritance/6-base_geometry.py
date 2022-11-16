0849a80e8d335f61889388b68b21939e13a6c90b
RuthBiney <r.biney@alustudent.com>
2022-11-16 03:33:43 -0800

    base2

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""BaseGeometry class"""
+
+
+class BaseGeometry:
+    """Class Geometry"""
+    def area(self):
+        """Raises an exception when called"""
+        raise Exception('area() is not implemented')
