f523b8a4ae6d141772ba6167b4fb20f53586e8e0
RuthBiney <r.biney@alustudent.com>
2022-11-22 14:22:32 -0800

    email

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
 """Send POST"""
+
 import urllib.request
 import urllib.parse
 import sys
 
-
-def sender():
-    """sender"""
-    v = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(v)
-    data = data.encode("ascii")
-    req = urllib.request.Request(sys.argv[1], data)
-    with urllib.request.urlopen(req) as response:
-        html = response.read()
-        print(html.decode("utf-8"))
-
-if __name__ == "__main__":
-    sender()
+if __name__ == '__main__':
+    """"           """
+    url = sys.argv[1]
+    message = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(message)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as reply:
+        content = reply.read()
+        print("{}".format(content.decode("utf-8")))
