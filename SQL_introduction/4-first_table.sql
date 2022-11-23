ffb645ba4ed2c5ff8abfff5335843b81760cb980
RuthBiney <r.biney@alustudent.com>
2022-11-23 13:28:41 -0800

    first table

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,6 +1,5 @@
--- creates a table called first_table
--- in the current database
+-- creates a table called first_table in the current database
 CREATE TABLE IF NOT EXISTS first_table(
-    id INT
-    VARCHAR(256)
+    id INT,
+    name VARCHAR(256)
 );
