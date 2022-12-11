75e44ad19b01e030684515d24c8c96d78b5c2f8f
RuthBiney <r.biney@alustudent.com>
2022-12-11 04:42:47 -0800

    all

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -2,20 +2,12 @@
 """class definition of a city"""
 
 import sqlalchemy
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sys import argv
+from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base, State
 
-if __name__ == "__main__":
-    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
-                                                                    argv[2],
-                                                                    argv[3]))
-    Base.metadata.create_all(eng)
-    Session = sessionmaker(bind=eng)
-    session = Session()
-    states = session.query(State).filter(State.name.like('%a%'))
-    for state in states:
-        session.delete(state)
-    session.commit()
-    session.close()
+class City(Base):
+    """Display of a city"""
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
