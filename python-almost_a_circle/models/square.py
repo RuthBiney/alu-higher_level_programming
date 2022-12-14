917bd1e99b0eb373cb3301f07cb57078624e8ab2
RuthBiney <r.biney@alustudent.com>
2022-12-14 20:27:45 +0200

    done

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -49,6 +49,7 @@ class Square(Rectangle):
                 self.x = kwargs["x"]
             if "y" in kwargs:
                 self.y = kwargs["y"]
+
     def to_dictionary(self):
         """presenting the square"""
         d = {}
