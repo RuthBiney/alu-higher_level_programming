5c8153cf876ac557526bc869bb7b173fd5e4f185
RuthBiney <r.biney@alustudent.com>
2022-12-14 21:06:27 +0200

    done

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,177 +1,75 @@
 #!/usr/bin/python3
 """testing the base class"""
 
+import os
 import unittest
-import inspect
-import pep8
-import json
-from models import base
-Base = base.Base
-
-
-class TestBaseDocs(unittest.TestCase):
-    """test the base class"""
-    @classmethod
-    def setUpClass(cls):
-        """setting up the test"""
-        cls.base_funcs = inspect.getmembers(Base, inspect.isfunction)
-
-    def test_pep8_conformance_base(self):
-        """conforming to pep8"""
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['models/base.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
-
-    def test_pep8_conformance_test_base(self):
-        """confirmation of pep8"""
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['tests/test_models/test_base.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
-
-    def test_module_docstring(self):
-        """testing the module"""
-        self.assertTrue(len(base.__doc__) >= 1)
-
-    def test_class_docstring(self):
-        """testing the base class"""
-        self.assertTrue(len(Base.__doc__) >= 1)
-
-    def test_func_docstrings(self):
-        """testing the docstring"""
-        for func in self.base_funcs:
-            self.assertTrue(len(func[1].__doc__) >= 1)
+from models.rectangle import Rectangle
+from models.base import Base
+from models.square import Square
 
 
 class TestBase(unittest.TestCase):
-    """tests to check how the base class function"""
-    def test_too_many_args(self):
-        """test for many args to init"""
-        with self.assertRaises(TypeError):
-            b = Base(1, 1)
+    """Test class for base"""
 
-    def test_no_id(self):
-        """testing for id"""
-        b = Base()
-        self.assertEqual(b.id, 1)
-
-    def test_id_set(self):
-         """testing for id"""
-         b98 = Base(98)
-         self.assertEqual(b98.id, 98)
-
-    def test_no_id_after_set(self):
-        """testing fir none"""
-        b2 = Base()
-        self.assertEqual(b2.id, 2)
-
-    def test_nb_private(self):
-        """private instance attribute"""
-        b = Base(3)
-        with self.assertRaises(AttributeError):
-            print(b.nb_objects)
-        with self.assertRaises(AttributeError):
-            print(b.__nb_objects)
+    def test_id(self):
+        """Test for id"""
+        d1 = Base()
+        self.assertEqual(d1.id, 1)
+        d2 = Base()
+        self.assertEqual(d2.id, 2)
+        d3 = Base(89)
+        self.assertEqual(d3.id, 89)
 
     def test_to_json_string(self):
-        """test for JSON string"""
-        Base._Base__nb_objects = 0
-        d1 = {"id": 9, "width": 5, "height": 6, "x": 7, "y": 8}
-        d2 = {"id": 2, "width": 2, "height": 3, "x": 4, "y": 0}
-        json_s = Base.to_json_string([d1, d2])
-        self.assertTrue(type(json_s) is str)
-        d = json.loads(json_s)
-        self.assertEqual(d, [d1, d2])
-
-    def test_empty_to_json_string(self):
-        """test for none"""
-        json_s = Base.to_json_string([])
-        self.assertTrue(type(json_s) is str)
-        self.assertEqual(json_s, "[]")
-
-    def test_None_to_json_String(self):
-        json_s = Base.to_json_string(None)
-        self.assertTrue(type(json_s) is str)
-        self.assertEqual(json_s, "[]")
+        """test to_json_string"""
+        self.assertEqual(Base.to_json_string(None), "[]")
+        self.assertEqual(Base.to_json_string([]), "[]")
+        self.assertEqual(Base.to_json_string([{'id': 12}]), '[{"id": 12}]')
+        self.assertEqual(type(Base.to_json_string([{'id': 12}])), str)
 
     def test_from_json_string(self):
-        """testing from JSON string"""
-        json_str = '[{"id": 9, "width": 5, "height": 6, "x": 7, "y": 8}, \
-{"id": 2, "width": 2, "height": 3, "x": 4, "y": 0}]'
-        json_l = Base.from_json_string(json_str)
-        self.assertTrue(type(json_l) is list)
-        self.assertEqual(len(json_l), 2)
-        self.assertTrue(type(json_l[0]) is dict)
-        self.assertTrue(type(json_l[1]) is dict)
-        self.assertEqual(json_l[0],
-                         {"id": 9, "width": 5, "height": 6, "x": 7, "y": 8})
-        self.assertEqual(json_l[1],
-                         {"id": 2, "width": 2, "height": 3, "x": 4, "y": 0})
+        """test from_json_string"""
+        self.assertEqual(Base.from_json_string(None), [])
+        self.assertEqual(Base.from_json_string("[]"), [])
+        self.assertEqual(Base.from_json_string('[{"id": 89}]'), [{'id': 89}])
+        self.assertEqual(type(Base.from_json_string('[{"id": 12}]')), list)
 
-    def test_fjs_empty(self):
-        """empty string"""
-        self.assertEqual([], Base.from_json_string(""))
+    def test_save_to_file(self):
+        """test save to file"""
+        Base._Base__nb_objects = 0
 
-    def test_fjs_None(self):
-        """empty string"""
-        self.assertEqual([], Base.from_json_string(None))
+        Square.save_to_file(None)
+        self.assertTrue(os.path.isfile("Square.json"))
+        with open("Square.json", "r") as fall:
+            self.assertEqual(fall.read(), "[]")
 
+        Square.save_to_file([])
+        with open("Square.json", "r") as fall:
+            self.assertEqual(fall.read(), "[]")
+            self.assertEqual(type(f.read()), str)
 
+        Square.save_to_file([Square(1)])
+        with open("Square.json", "r") as fall:
+            self.assertEqual(fall.read(), 
+            '[{"id": 1, "size": 1, "x": 0, "y": 0}]')
+            Base._Base__nb_objects = 0
 
+        Rectangle.save_to_file(None)
+        self.assertTrue(os.path.isfile("Rectangle.json"))
+        with open("Rectangle.json", "r") as fall:
+            self.assertEqual(f.read(), "[]")
 
+        Rectangle.save_to_file([])
+        with open("Rectangle.json", "r") as fall:
+            self.assertEqual(fall.read(), "[]")
+            self.assertEqual(type(f.read()), str)
 
+        os.remove("Rectangle.json")
+        Rectangle.save_to_file([Rectangle(1, 2)])
+        with open("Rectangle.json", "r") as f:
+            self.assertEqual(f.read(), 
+                             '[{"id": 1, "width": 1, '
+                             '"height": 2, "x": 0, "y": 0}]')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    unittest.main()
