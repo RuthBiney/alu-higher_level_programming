406e42ed06eb2e50423bd43c8bee13bcf9789b11
RuthBiney <r.biney@alustudent.com>
2023-02-03 07:22:52 -0800

    L love C

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
-Can first argument be converted to an integer?
+// Can first argument be converted to an integer?
 if (isNaN(parseInt(args[0]))) {
   console.log('Missing number of occurrences');
 } else {
