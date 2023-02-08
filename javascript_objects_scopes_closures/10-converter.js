7e6788cd98bb52f18abbac43bc0229460f58f47a
RuthBiney <r.biney@alustudent.com>
2023-02-08 05:40:44 -0800

    number conversion

---
+++ javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+//function that convert a number from base 10 to another base
+exports.converter = function (base) {
+  return function (num) {
+    return num.toString(base);
+  };
+};
