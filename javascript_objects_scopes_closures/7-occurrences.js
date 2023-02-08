cd84aceedb791f61e1bf68492f5ff7a87e438ae1
RuthBiney <r.biney@alustudent.com>
2023-02-08 05:23:30 -0800

    Occurrences

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-//A function that returns the number of occurrences in a list
+// A function that returns the number of occurrences in a list
 exports.nbOccurences = function (list, searchElement) {
   let occurences = 0;
   list.forEach(element => {
