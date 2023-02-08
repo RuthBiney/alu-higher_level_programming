d6227879d16be7f402e8baf1644204c73d8b55b1
RuthBiney <r.biney@alustudent.com>
2023-02-08 05:30:11 -0800

    Esrever

---
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+//Function that return the reversed version of a list
+exports.esrever = function (list) {
+  const output = [];
+  let i = list.length - 1;
+  for (i; i >= 0; i--) {
+    output.push(list[i]);
+  }
+  return output;
+};
