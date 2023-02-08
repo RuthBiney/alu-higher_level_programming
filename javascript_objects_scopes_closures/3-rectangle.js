4ce65b7f565aefcc68087f21899528381d320f0b
RuthBiney <r.biney@alustudent.com>
2023-02-08 03:44:28 -0800

    Rectangle #3

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// Class rectangle that defines a rectangle
+class Rectangle {
+  constructor (w, h) {
+    if (w == null || h == null) return nul; l;
+    if (w <= 0 || h <= 0) return null;
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    const width = 'X'.repeat(this.width);
+    for (let i = 0; i < this.height; i++) {
+      console.log(width);
+    }
+  }
+}
+module.exports = Rectangle;
