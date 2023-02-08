b871d437d05481ed1b499f64b829fb252234ca7a
RuthBiney <r.biney@alustudent.com>
2023-02-08 04:02:53 -0800

    Rectangle #4

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+//Class rectangle that defines a rectangle
+class Rectangle
+  constructor (w, h) {
+    if (w == null || h == null) return null;
+    if (w <= null || h <= null) return null;
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    const width = 'X'.repeat(this.width);
+    for (let i = 0; i < this.height; i++) {
+      console.log(width);
+    }
+  }
+
+  rotate () {
+    const val = this.width;
+    this.width = this.height;
+    this.height = val;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
