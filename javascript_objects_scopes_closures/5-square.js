6e357c81bfad6ae8596a7b39c22f104944310596
RuthBiney <r.biney@alustudent.com>
2023-02-08 05:05:15 -0800

    Square #0

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const Rectangle = require('./4-rectangle');
-//A class square that defines a square and inherit from rectangle
+// A class square that defines a square and inherit from rectangle
 class Square extends Rectangle {
   constructor (size) {
     super(size, size);
