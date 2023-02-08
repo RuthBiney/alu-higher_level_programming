c95cabb3d9244d788cf126ef0f3b8c2535f5d3e9
RuthBiney <r.biney@alustudent.com>
2023-02-08 03:27:45 -0800

    Rectangle #2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-//Class rectangle that defines a rectangle
+// Class rectangle that defines a rectangle
 class Rectangle {
-  constructor  (w, h) {
+  constructor (w, h) {
     if (w == null || h == null) return null;
-    if (w <= null || h<= null) return null;
+    if (w <= null || h <= null) return null;
     this.width = w;
-    this .height = h;
+    this.height = h;
   }
 }
 module.exports = Rectangle;
