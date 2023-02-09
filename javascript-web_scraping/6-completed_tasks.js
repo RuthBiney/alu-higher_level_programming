975e9f68feac32364a19e9e5212dde6513433512
RuthBiney <r.biney@alustudent.com>
2023-02-09 03:35:15 -0800

    How many completed?

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -9,11 +9,11 @@ request(apiUrl, (error, response, body) => {
     const parseData = JSON.parse(body);
     parseData.forEach(item => {
       if (item.completed) {
-	if (result[item.userId] === undefined) {
+        if (result[item.userId] === undefined) {
           result[item.userId] = 1;
         } else {
           result[item.userId] += 1;
-	}
+        }
       }
     });
     console.log(result);
